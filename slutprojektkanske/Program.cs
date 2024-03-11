40a414b136f52a3a643772d1c537a006d4604f0b
Ludwig Langell <ludwig.langell@elev.ga.ntig.se>
2024-03-11 09:32:18 +0100

    boren kan inte gå

--- slutprojektkanske/Program.cs
+++ slutprojektkanske/Program.cs
@@ -3,17 +3,19 @@ using System.Numerics;
 using Raylib_cs;
 
 string Scene = "Cavern";
+int PlayerX = 520;
+int PlayerY = 400;
 Vector2 VectorBackground = new(0, 0);
+Vector2 PlayerVector= new(PlayerX,PlayerY);
 
 Raylib.InitWindow(1200, 900, "The Thing");
 Raylib.SetTargetFPS(60);
 
 Texture2D Scene1 = Raylib.LoadTexture("Cavern.png");
 Texture2D Scene2 = Raylib.LoadTexture("ExitFromCavern.png");
+Texture2D BoreTexture = Raylib.LoadTexture("Bore.png");
 // to be moved1
 
-int PlayerX = 520;
-int PlayerY = 400;
 
 // to be moved2
 
@@ -21,14 +23,21 @@ while (!Raylib.WindowShouldClose())
 {
     // to be moved if can 1
 
-
+/*
+Console.WriteLine(PlayerY);
+Console.WriteLine(PlayerX);
+*/
     Rectangle Border1 = new(0, 0, 1200, 100);
     Rectangle Border2 = new(1000, 100, 200, 150);
     Rectangle Border3 = new(0, 100, 150, 1100);
     Rectangle Border4 = new(150, 710, 1040, 190);
     Rectangle Border5 = new(1050, 500, 150, 400);
-    Rectangle NewArea1 = new(1135, 150, 65, 450);
+    Rectangle CaveExit = new(1135, 150, 65, 450);
     Rectangle Player = new(PlayerX, PlayerY, 100, 100);
+    Rectangle Bore = new(PlayerX, PlayerY, BoreTexture.Width,BoreTexture.Height);
+
+    Rectangle CaveEntrance=new(10,400,100,100);
+
 
     // to be moved if can 2
 
@@ -100,15 +109,20 @@ while (!Raylib.WindowShouldClose())
         PlayerY = 400;
     }
     }
-    if (Raylib.CheckCollisionRecs(Player, NewArea1))
+    if (Raylib.CheckCollisionRecs(Player, CaveExit))
     {
         Scene = "ExitFromCavern";
-        PlayerX = 100;
+        PlayerX = 130;
         PlayerY = 400;
+    }
+    if (Raylib.CheckCollisionRecs(Player, CaveEntrance))
+    {
+        Scene = "Cavern";
+        PlayerX = 1000;
+        PlayerY = 340;
     }
     // to be moved2
 }
-
 
 
 
@@ -123,12 +137,16 @@ void levels()
     Rectangle Border3 = new(0, 100, 150, 1100);
     Rectangle Border4 = new(150, 710, 1040, 190);
     Rectangle Border5 = new(1050, 500, 150, 400);
-    Rectangle NewArea1 = new(1135, 150, 65, 450);
+    Rectangle CaveExit = new(1135, 150, 65, 450);
     Rectangle Player = new(PlayerX, PlayerY, 100, 100);
     Rectangle Goblingo = new(200,200,150,150);
     Rectangle Ghoul=new(400,400,90,90);
+    Rectangle CaveEntrance=new(10,400,100,100);
+    Rectangle Bore = new(PlayerX, PlayerY, BoreTexture.Width,BoreTexture.Height);
+
     Raylib.DrawRectangleRec(Player, Color.Black);
     Raylib.ClearBackground(Color.RayWhite);
+    Raylib.DrawTextureRec(BoreTexture,Bore,PlayerVector,Color.RayWhite);
 
 
     if (Scene == "Cavern")
@@ -138,12 +156,13 @@ void levels()
         Raylib.DrawRectangleRec(Border3, Color.Blank);
         Raylib.DrawRectangleRec(Border4, Color.Blank);
         Raylib.DrawRectangleRec(Border5, Color.Blank);
-        Raylib.DrawRectangleRec(NewArea1, Color.Blank);
+        Raylib.DrawRectangleRec(CaveExit, Color.Blank);
     }
     if (Scene == "ExitFromCavern")
     {
         Raylib.DrawRectangleRec(Goblingo,Color.DarkGreen);
         Raylib.DrawRectangleRec(Ghoul,Color.SkyBlue);
+        Raylib.DrawRectangleRec(CaveEntrance,Color.Blank);
 
      
 
